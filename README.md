54fc8d0d0b06b118363d49b8973fc5689b49b8a5
laughforever <1849683708@qq.com>
2020-06-01 16:18:22 +0800

    练习1.1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FullStackCourse2020
-深入浅出现代Web编程       https://fullstackopen.com/zh/
+深入浅出现代Web编程
+https://fullstackopen.com/zh/
+___
+## part1
+创建一个名为part1的应用，并进入到它的目录：
+```
+npx create-react-app part1
+cd part1
+```
+让应用运行起来：`npm start`
+
+默认情况下，应用在本地localhost，3000端口运行，地址为 http://localhost:3000
+
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+//定义组件，组件的首字母必须大写
+const Header = (props) => (
+  <div>
+    <p>
+      课程的名字是{props.course}
+    </p>
+  </div>
+)
+
+const Content = (props) => (
+  <div>
+    <p>
+      课程Part1：{props.name},练习题有{props.num}个。
+    </p>
+  </div>
+)
+
+const Total = (props) => (
+  <div>
+    <p>
+      练习题总数为：{props.num}
+    </p>
+  </div>
+)
+
+const App = () => {
+  const course = 'Full Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content name={part1} num={exercises1}/>
+      <Content name={part2} num={exercises2}/>
+      <Content name={part3} num={exercises3}/>
+      <Total num={exercises1+exercises2+exercises3}/>
+    </div>
+  )
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +60,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
